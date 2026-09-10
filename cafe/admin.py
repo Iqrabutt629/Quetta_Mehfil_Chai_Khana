f83faf10571645cda13b7b59de450c2a40cd931b
Iqrabutt629 <iqra.butt9157@gmail.com>
2026-09-10 13:08:53 +0500

    Updated admin panel to show payment status

--- cafe/admin.py
+++ cafe/admin.py
@@ -8,12 +8,16 @@ admin.site.register(MenuItem)
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     
-    list_display = ('id', 'customer_name', 'order_list', 'total_amount', 'picking_time', 'phone')
+    list_display = ('id', 'customer_name', 'total_amount', 'amount_paid', 'pending_amount', 'payment_method', 'payment_type', 'picking_time', 'phone')
     
     
-    list_filter = ('picking_time',)
+    list_filter = ('picking_time', 'payment_method')
     
     
     search_fields = ('customer_name', 'phone')
 
+    def pending_amount(self, obj):
+        return obj.total_amount - obj.amount_paid
+    pending_amount.short_description = 'Pending Amount'
+    
 admin.site.register(Deal)
